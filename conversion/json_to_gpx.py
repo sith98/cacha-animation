e19074d9e05787b301df3847133e79f7a5bf7fa8
sith98 <sith98@web.de>
2024-05-23 23:59:12 +0200

    Add hunter/runaway GPX files + creation script

--- conversion/json_to_gpx.py
+++ conversion/json_to_gpx.py
@@ -7,9 +7,23 @@ import datetime
 json_dir = "data/log-by-user"
 
 
+def parse_team_caught():
+    with open("data/log-export/team_caught.json") as file:
+        data = json.load(file)
+
+    catch_times = {}
+    for entry in data:
+        catch_times[entry["runaway_active_user"]] = datetime.datetime.fromisoformat(
+            entry["timestamp"] + "Z"
+        ).timestamp()
+
+    catch_times[data[-1]["hunter_active_user"]] = 0.0
+    return catch_times
+
+
 def main():
-    gpx = gpxpy.gpx.GPX()
-    gpx.name = "combined"
+    catch_times = parse_team_caught()
+    all_points = []
     for file_name in os.listdir(json_dir):
         full_name = os.path.join(json_dir, file_name)
         user_id = os.path.splitext(file_name)[0]
@@ -25,25 +39,51 @@ def main():
         gpx_segment = gpxpy.gpx.GPXTrackSegment()
         gpx_track.segments.append(gpx_segment)
 
+        points = []
+        hunter_points = []
+        runaway_points = []
+
         # Create points:
         for d in data:
             location = d["current_location"]
-            gpx_segment.points.append(
-                gpxpy.gpx.GPXTrackPoint(
-                    location["lat"],
-                    location["lon"],
-                    time=datetime.datetime.fromtimestamp(
-                        location["timestamp"] / 1000, datetime.UTC
-                    ),
-                )
+            time = datetime.datetime.fromtimestamp(
+                location["timestamp"] / 1000, datetime.UTC
             )
-        gpx.tracks.append(gpx_track)
-        user_gpx.tracks.append(gpx_track)
+            point = gpxpy.gpx.GPXTrackPoint(
+                location["lat"],
+                location["lon"],
+                time=time,
+            )
+            points.append(point)
+            if user_id not in catch_times or catch_times[user_id] > time.timestamp():
+                runaway_points.append(point)
+            else:
+                hunter_points.append(point)
 
-        user_gpx.name = user_id
-        with open(f"data/gpx/{user_id}.gpx", "w") as file:
-            file.write(user_gpx.to_xml())
-    with open(f"data/gpx/combined.gpx", "w") as file:
+        write_gpx([points], user_id, f"data/gpx/{user_id}.gpx")
+        write_gpx(
+            [hunter_points], f"hunter_{user_id}", f"data/gpx/hunter/{user_id}.gpx"
+        )
+        write_gpx(
+            [runaway_points], f"runaway_{user_id}", f"data/gpx/runaway/{user_id}.gpx"
+        )
+        all_points.append(points)
+    write_gpx(all_points, "combined", "data/gpx/combined.gpx")
+
+
+def write_gpx(point_lists, name, url):
+    gpx = gpxpy.gpx.GPX()
+    gpx.name = name
+    # Create first track in our GPX:
+    for point_list in point_lists:
+        gpx_track = gpxpy.gpx.GPXTrack()
+        gpx.tracks.append(gpx_track)
+
+        # Create first segment in our GPX track:
+        gpx_segment = gpxpy.gpx.GPXTrackSegment()
+        gpx_track.segments.append(gpx_segment)
+        gpx_segment.points.extend(point_list)
+    with open(url, "w") as file:
         file.write(gpx.to_xml())
 
 
