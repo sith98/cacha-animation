2fb7cdd9b118269029705f0ed43e4a25ff49fc87
sith98 <sith98@web.de>
2024-05-24 19:59:36 +0200

    Split JSON for hackaburg-campuswiese

--- conversion/split_teams.py
+++ conversion/split_teams.py
@@ -1,16 +1,19 @@
 import json
+import os
 
-with open("data/log-export/regular_status_update.json") as file:
-    data = json.load(file)
 
-by_user = {}
+for subdir in os.listdir("data/"):
+    with open(f"data/{subdir}/log-export/regular_status_update.json") as file:
+        data = json.load(file)
 
-for entry in data:
-    user = entry["active_user"]
-    if user not in by_user:
-        by_user[user] = []
-    by_user[user].append(entry)
+    by_user = {}
 
-for user, data in by_user.items():
-    with open(f"data/log-by-user/{user}.json", "w") as file:
-        json.dump(data, file, indent=4)
+    for entry in data:
+        user = entry["active_user"]
+        if user not in by_user:
+            by_user[user] = []
+        by_user[user].append(entry)
+
+    for user, data in by_user.items():
+        with open(f"data/{subdir}/log-by-user/{user}.json", "w") as file:
+            json.dump(data, file, indent=4)
