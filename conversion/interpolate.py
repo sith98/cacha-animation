53037a560fe9f647424306624d71cc1e11b23309
jippiee <jippiee@yahoo.com>
2024-05-25 11:35:57 +0200

    Interpolate: Add cumulative distances

--- conversion/interpolate.py
+++ conversion/interpolate.py
@@ -1,11 +1,14 @@
 import numpy as np
 import pandas as pd
 from scipy.interpolate import PchipInterpolator
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import seaborn as sns
 import os
 import json
 import copy
 from pathlib import Path
+
+sns.set_theme()
 
 def main(time_step_ms, inactive_after_ms):
     for game_name in os.listdir("data/"):
@@ -20,6 +23,7 @@ def main(time_step_ms, inactive_after_ms):
         time_equi = interpol_dataframe.index
         connection_dataframe = connection_status(time_equi, teams, inactive_after_ms)
         connection_dataframe.to_parquet(f"data/{game_name}/log-interpol/connection.parquet")
+        cumulative_distance = make_cumulative_distance(interpol_dataframe, teams)
         # Write back to json
         result_json = []
         for team_json in os.listdir(f"data/{game_name}/log-by-user/"):
@@ -33,6 +37,7 @@ def main(time_step_ms, inactive_after_ms):
                 next_json_entry = copy.deepcopy(json_data[current_json_index])
                 next_json_entry["is_connection_active"] = True
                 next_json_entry["is_interpolated"] = False
+                next_json_entry["cumulative_distance"] = 0.0
                 result_json.append(next_json_entry)
                 current_json_index += 1
             for t in time_equi:
@@ -40,6 +45,7 @@ def main(time_step_ms, inactive_after_ms):
                     next_json_entry = copy.deepcopy(json_data[current_json_index])
                     next_json_entry["is_connection_active"] = True
                     next_json_entry["is_interpolated"] = False
+                    next_json_entry["cumulative_distance"] = float(result_json[-1]["cumulative_distance"])
                     result_json.append(next_json_entry)
                     current_json_index += 1
                 assert json_data[current_json_index]["current_location"]["timestamp"] <= t
@@ -49,16 +55,17 @@ def main(time_step_ms, inactive_after_ms):
                 next_json_entry["current_location"]["lon"] = interpol_dataframe[team_name, "lon"][t]
                 next_json_entry["is_connection_active"] = bool(connection_dataframe[team_name][t])
                 next_json_entry["is_interpolated"] = True
+                next_json_entry["cumulative_distance"] = float(cumulative_distance[team_name][t])
                 result_json.append(next_json_entry)
             while current_json_index+1 < len(json_data):
                 next_json_entry = copy.deepcopy(json_data[current_json_index])
                 next_json_entry["is_connection_active"] = True
                 next_json_entry["is_interpolated"] = False
+                next_json_entry["cumulative_distance"] = float(result_json[-1]["cumulative_distance"])
                 result_json.append(next_json_entry)
                 current_json_index += 1
         with open(f"data/{game_name}/log-interpol/interpol.json", "w") as file:
             json.dump(result_json, file, indent=4)
-
 
 def json_to_dataframe(path):
     with open(path) as file:
@@ -114,6 +121,32 @@ def connection_status(time_equi, teams, inactive_after_ms):
         #plt.show()
         connection_dataframe[team_name] = active_connection
     return connection_dataframe
+
+def make_cumulative_distance(interpol_dataframe, teams):
+    #print(interpol_dataframe.head())
+    earth_radius_in_meters = 6_371_008.8
+    df = pd.DataFrame(index=interpol_dataframe.index, columns=teams.keys())
+    for team_name in teams.keys():
+        lat = np.deg2rad(interpol_dataframe[team_name]["lat"].to_numpy())
+        lon = np.deg2rad(interpol_dataframe[team_name]["lon"].to_numpy())
+        x = earth_radius_in_meters * np.cos(lat) * np.cos(lon)
+        y = earth_radius_in_meters * np.cos(lat) * np.sin(lon)
+        z = earth_radius_in_meters * np.sin(lat)
+        diff_x = np.zeros_like(x)
+        diff_y = np.zeros_like(y)
+        diff_z = np.zeros_like(z)
+        diff_x[1:] = x[1:] - x[:-1]
+        diff_y[1:] = y[1:] - y[:-1]
+        diff_z[1:] = z[1:] - z[:-1]
+        distance = np.hypot(diff_x, np.hypot(diff_y, diff_z))
+        cum_distance = np.cumsum(distance)
+        #plt.plot(cum_distance)
+        df[team_name] = cum_distance
+    #sns.relplot(data=df, kind="line")
+    #plt.show()
+    #print(df.head())
+    return df
+
 
 if __name__ == "__main__":
     main(time_step_ms=5000, inactive_after_ms=30_000)
